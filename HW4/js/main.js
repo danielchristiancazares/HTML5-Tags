c9c92787539245ce5815c68801b4997fba974745
dbykkov <dbykkov@ucsd.edu>
2015-07-29 21:49:58 -0700

    Search should now be working. Formatted some files.

--- HW4/js/main.js
+++ HW4/js/main.js
@@ -6,7 +6,9 @@ else if (typeof Firebase == 'undefined')
 else {
 	// search utility
 	var SearchUtil = function (tagList) {
-		var list = [];
+		var that = this,
+			list = [];
+		this.ready = false;
 
 		tagList.on('value', function (xs) {
 			xs.forEach(function (x) {
@@ -14,9 +16,11 @@ else {
 					tagData = x.val(); // {category, description, example}
 				list.push(tagName);
 			});
+			that.ready = true;
 		});
 
 		this.search = function (query) {
+			
 			var results = [];
 			list.forEach(function (result) {
 				if (result.indexOf(query) != -1) {
@@ -32,24 +36,38 @@ else {
 		var searchTimeout,
 			body = $('body'),
 			searchBox = $('.navbar-right input.form-control'),
-			resultBox = $('<div class="search-result-box"></div>').appendTo(body),
-			resultList = $('<ul></ul>').appendTo(resultBox),
+			resultBox = $('<div class="search-result-box list-group"></div>').appendTo(body),
+			closeBtn = $('<a class="list-group-item list-group-item-info" href="#">Close<span class="glyphicon glyphicon-close"></span></a>').on('click', function () { 
+				resultBox.hide();
+			}).appendTo(resultBox),
 			tagList = new Firebase('https://boiling-heat-3594.firebaseio.com').child('tags'),
 			su = new SearchUtil(tagList);
 		// attach search function to the search input
 		searchBox.on('input', function (event) {
+			var query = searchBox.val();
+			if (query.length < 1)
+				return;
+			resultBox.find('a:not(.list-group-item-info)').remove();
 			clearTimeout(searchTimeout);
-			searchTimeout = setTimeout(function () {
+			
+			var doSearch = function () {
+				if (!su.ready) {
+					searchTimeout = setTimeout(doSearch, 250);
+					return;
+				}
 				var results = su.search(searchBox.val());
 				// show list
-				resultList.empty();
 				for (var result in results) {
 					var data = results[result],
-						li = $('<li></li>').appendTo(resultList),
-						link = $('<a></a>').text(data).attr('href', 'tag.html?id=' + data).appendTo(li);
+						link = $('<a class="list-group-item"></a>').appendTo(resultBox).text(data).attr('href', 'tag.html?id=' + data);
 				}
-
-			}, 250);
+				resultBox.show();
+				if (!results.length) {
+					$('<a class="list-group-item list-group-item-warning">No results found!</a>').appendTo(resultBox);
+				}
+			}
+			
+			searchTimeout = setTimeout(doSearch, 250);
 		});
 	});
 }
